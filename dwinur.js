fc5aaf72f1170e8dbd9caca9cf1ff62361e205e9
dwinurfatimah20 <118708533+dwinurfatimah20@users.noreply.github.com>
2022-11-29 11:38:45 +0700

    Add files via upload
    var input = require('readline-sync');
    var panjang = input.question("masukkan panjang =");
    var lebar = input.question("masukkan lebar =");
    console.log("Luas persegi panjang adalah ");
    luas=(panjang * lebar);
    console.log(luas);

---
+++ dwinur.js
@@ -0,0 +1,6 @@
+var input = require('readline-sync');
+var panjang = input.question("masukkan panjang =");
+var lebar = input.question("masukkan lebar =");
+console.log("Luas persegi panjang adalah ");
+luas=(panjang * lebar);
+console.log(luas);
